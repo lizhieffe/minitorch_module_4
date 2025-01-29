364cfb391c07047f3aab2bdec793f53b1c99013d
lizhieffe <lizhieffe@gmail.com>
2025-01-29 07:47:15 +0000

    colab update

--- minitorch/nn.py
+++ minitorch/nn.py
@@ -1,7 +1,8 @@
 from typing import Tuple
-from minitorch.tensor_data import index_to_position
+from minitorch.tensor_data import index_to_position, to_index
 
 import numpy as np
+from numpy._core.defchararray import title
 
 from . import operators
 from .autodiff import Context
@@ -41,15 +42,18 @@ def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
 
     input_storage, _, input_strides = input.tuple()
 
-    out_h = height / kh
-    out_w = width / kw
+    out_h = height // kh
+    out_w = width // kw
+
 
     out_tensor = input.zeros((batch, channel, out_h, out_w))
     out, out_shape, out_strides = out_tensor.tuple()
     out_size = batch * channel * out_h * out_w
 
+
     for i in range(out_size):
         out_idx = np.empty_like(out_shape)
+        to_index(i, out_shape, out_idx)
         bi, ci, ohi, owi = out_idx
 
         total = 0.0
@@ -62,7 +66,46 @@ def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
                 total += input_val
         total = total / kh / kw
 
-        out_pos = index_to_position(out_idx, out_shape)
+        out_pos_raw = out_idx[0] * out_strides[0] + out_idx[1] * out_strides[1] + out_idx[2] * out_strides[2] + out_idx[3] * out_strides[3]
+        out_pos = index_to_position(out_idx, out_strides)
         out[out_pos] = total
+    return out_tensor, out_h, out_w
+
+class AvgPool2d(Function):
+    @staticmethod
+    def forward(ctx: Context, t1: Tensor, kernel: Tensor) -> Tensor:
+        kernel = np.array(kernel.to_numpy(), dtype=np.int64)
+        ctx.save_for_backward(t1, kernel)
+        return tile(t1, kernel)[0]
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tensor:
+        (t1, kernel) = ctx.saved_tensors
+        
+        # Out here means the emit to previous layer.
+        out = t1.zeros(t1.shape)
+        out_storage, out_shape, out_strides = out.tuple()
+        
+        print(f"===lizhi {grad_output=}")
+        for i in range(out_storage.shape[0]):
+            out_idx = np.empty_like(out_shape)
+            to_index(i, out_shape, out_idx)
+
+            grad_storage, grad_shape, grad_strides = grad_output.tuple()
+            grad_idx = np.array(out_idx)
+            grad_idx[-2] = out_idx[-2] // kernel[0]
+            grad_idx[-1] = out_idx[-1] // kernel[1]
+            grad_pos = index_to_position(grad_idx, grad_strides)
+            grad_val = grad_storage[grad_pos]
+
+            out_pos = index_to_position(out_idx, out_strides)
+            out_storage[out_pos] = grad_val / kernel[0] / kernel[1]
+
+        return out
+
+
+def avgpool2d(input: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    kernel_tensor = input.make(kernel, (2,), backend=input.backend)
+    return AvgPool2d.apply(input, kernel_tensor)
 
 # TODO: Implement for Task 4.3.
