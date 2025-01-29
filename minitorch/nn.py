68277443d009d3d071953f2443108bb0739afe35
lizhieffe <lizhieffe@gmail.com>
2025-01-29 06:25:57 +0000

    colab update

--- minitorch/nn.py
+++ minitorch/nn.py
@@ -1,4 +1,7 @@
 from typing import Tuple
+from minitorch.tensor_data import index_to_position
+
+import numpy as np
 
 from . import operators
 from .autodiff import Context
@@ -19,7 +22,7 @@ from .tensor_functions import Function, rand, tensor
 
 
 def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
-    """Reshape an image tensor for 2D pooling
+    """Reshape an image tensor for 2D pooling with an average operation.
 
     Args:
     ----
@@ -35,8 +38,31 @@ def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
     kh, kw = kernel
     assert height % kh == 0
     assert width % kw == 0
-    # TODO: Implement for Task 4.3.
-    raise NotImplementedError("Need to implement for Task 4.3")
 
+    input_storage, _, input_strides = input.tuple()
+
+    out_h = height / kh
+    out_w = width / kw
+
+    out_tensor = input.zeros((batch, channel, out_h, out_w))
+    out, out_shape, out_strides = out_tensor.tuple()
+    out_size = batch * channel * out_h * out_w
+
+    for i in range(out_size):
+        out_idx = np.empty_like(out_shape)
+        bi, ci, ohi, owi = out_idx
+
+        total = 0.0
+        for khi in range(kh):
+            for kwi in range(kw):
+                input_idx = np.array((bi, ci, ohi * kh + khi, owi * kw + kwi))
+                input_pos = index_to_position(input_idx, input_strides)
+                input_val = input_storage[input_pos]
+
+                total += input_val
+        total = total / kh / kw
+
+        out_pos = index_to_position(out_idx, out_shape)
+        out[out_pos] = total
 
 # TODO: Implement for Task 4.3.
