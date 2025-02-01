986a085498b2576c94c81e1ba29ab0827b46734d
lizhieffe <lizhieffe@gmail.com>
2025-02-01 06:05:46 +0000

    colab update

---
+++ minitorch/cuda_conv.py
@@ -0,0 +1,44 @@
+# type: ignore
+# Currently pyright doesn't support numba.cuda
+
+from typing import Callable, Optional, TypeVar, Any
+
+import numba
+from numba import cuda
+from numba.cuda import jit as _jit
+from .tensor import Tensor
+from .tensor_data import (
+    MAX_DIMS,
+    Shape,
+    Storage,
+    Strides,
+    TensorData,
+    broadcast_index,
+    index_to_position,
+    shape_broadcast,
+    to_index,
+)
+from .tensor_ops import MapProto, TensorOps
+
+FakeCUDAKernel = Any
+
+# This code will CUDA compile fast versions your tensor_data functions.
+# If you get an error, read the docs for NUMBA as to what is allowed
+# in these functions.
+
+Fn = TypeVar("Fn")
+
+
+def device_jit(fn: Fn, **kwargs) -> Fn:
+    return _jit(device=True, **kwargs)(fn)  # type: ignore
+
+
+def jit(fn, **kwargs) -> FakeCUDAKernel:
+    return _jit(**kwargs)(fn)  # type: ignore
+
+
+to_index = device_jit(to_index)
+index_to_position = device_jit(index_to_position)
+broadcast_index = device_jit(broadcast_index)
+
+THREADS_PER_BLOCK = 32
