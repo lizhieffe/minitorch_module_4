6d3a3086639ed0085915606897d149ec3c4caa30
lizhieffe <lizhieffe@gmail.com>
2025-02-02 05:09:11 +0000

    colab update

--- minitorch/cuda_conv.py
+++ minitorch/cuda_conv.py
@@ -58,19 +58,51 @@ def _tensor_conv1d(
 ) -> None:
     """
 
+  
     The grid x, y, z axis corresponds to the [B, T, COUT] dim of the output.
 
 
     """
+
+    # Get shapes
+    #
+    # input: `batch, in_channels, width`
+    # weight: `out_channels, in_channels, k_width`
+    # output: `batch, out_channels, width`
+    #
+    batch, in_channels, width = input_shape
+    out_channels, _, k_width = weight_shape
+    assert batch == out_shape[0]
+    assert out_channels == out_shape[1]
+    assert width == out_shape[2]
+
+    # TODO: why
+    input_batch_stride = input_strides[0] if input_shape[0] > 1 else 0
+
     # get the index in the grid
+    # [B, T, COUT]
     i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
     j = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
     k = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
 
+    if i >= batch or j >= width:
+        return
+
     # Allocate shared memory.
     BLOCK_DIM = 32
-    input_shared = cuda.shared.array((BLOCK_DIM, BLOCK_DIM, BLOCK_DIM), numba.float64)
-    weight_shared = cuda.shared.array((BLOCK_DIM, BLOCK_DIM, BLOCK_DIM), numba.float64)
+    input_shared = cuda.shared.array((BLOCK_DIM, BLOCK_DIM, BLOCK_DIM), numba.float64) # [B, T, KW * C_IN]
+    weight_shared = cuda.shared.array((BLOCK_DIM, BLOCK_DIM), numba.float64) # [KW * C_IN, C_OUT]
+
+    for conv_i in range(0, k_width * in_channels, BLOCK_DIM):
+        k_width_i = conv_i // in_channels
+        in_channels_i = conv_i % in_channels
+
+        input_index = (batch, in_channels_i, width + k_width_i)
+        input_shared[i][j][conv_i] = input[input_batch_stride * batch + input_strides[1] * in_channels_i + input_strides[2] * (width + k_width_i)]
+        weight_shared[conv_i][k] = weight[weight_strides[0] * k + weight_strides[1] * in_channels_i + weight_strides[2] * k_width_i]
+
+        if i < batch and j < width and k < k_width * in_channels:
+            input_shared[i][j]
 
     
 
